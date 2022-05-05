f2c3e5bcc627a30aa0c5645bee437c52f65fe374
petersh6 <petershengwhu@gmail.com>
2022-05-05 11:45:03 +0800

    fix

--- dgnn/build_graph.py
+++ dgnn/build_graph.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import torch
 from .dynamic_graph import DynamicGraph
+import os
 
-def load_graph(d: str) -> pd.DataFrame:
-    df = pd.read_csv('/home/gmsheng/repos/dynamic-graph-neural-network/dgnn/data/{}/edges.csv'.format(d))
+def load_graph(dataset: str) -> pd.DataFrame:
+    dir = os.path.dirname(__file__)
+    df = pd.read_csv(dir + '/data/{}/edges.csv'.format(dataset))
     return df
 
 def build_dynamic_graph(dataset: str = "REDDIT", block_size: int = 1024) -> DynamicGraph:
@@ -13,9 +15,9 @@ def build_dynamic_graph(dataset: str = "REDDIT", block_size: int = 1024) -> Dyna
     train_edge_end = df[df['ext_roll'].gt(0)].index[0]
     val_edge_end = df[df['ext_roll'].gt(1)].index[0]
    
-    train_src = torch.tensor(df['src'][:train_edge_end], dtype=int)
-    train_dst = torch.tensor(df['dst'][:train_edge_end], dtype=int)
-    train_ts = torch.tensor(df['time'][:train_edge_end]) # float64
+    train_src = torch.tensor(df['src'][:train_edge_end], dtype=torch.long)
+    train_dst = torch.tensor(df['dst'][:train_edge_end], dtype=torch.long)
+    train_ts = torch.tensor(df['time'][:train_edge_end], dtype=torch.float32)
 
     dgraph = DynamicGraph(block_size=block_size)
     dgraph.add_edges(train_src, train_dst, train_ts)
