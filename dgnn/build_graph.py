c71d8d9ab3208877dc949c926aab245162b57a57
petersh6 <petershengwhu@gmail.com>
2022-05-05 10:50:58 +0800

    Test build dynamic graph

--- dgnn/build_graph.py
+++ dgnn/build_graph.py
@@ -9,13 +9,10 @@ def load_graph(d: str) -> pd.DataFrame:
 
 def build_dynamic_graph(dataset: str = "REDDIT", block_size: int = 1024) -> DynamicGraph:
     block_size = 1024
-    batch_size = 600
     df = load_graph(dataset)
     train_edge_end = df[df['ext_roll'].gt(0)].index[0]
     val_edge_end = df[df['ext_roll'].gt(1)].index[0]
-    group_indexes = list()
-    group_indexes.append(np.array(df[:train_edge_end].index // batch_size))
-
+   
     train_src = torch.tensor(df['src'][:train_edge_end], dtype=int)
     train_dst = torch.tensor(df['dst'][:train_edge_end], dtype=int)
     train_ts = torch.tensor(df['time'][:train_edge_end]) # float64
--- tests/test_build_graph.py
+++ tests/test_build_graph.py
@@ -1,7 +1,9 @@
 from distutils.command.build import build
 import unittest
+import numpy as np
+import torch
 
-from dgnn.build_graph import build_dynamic_graph
+from dgnn.build_graph import build_dynamic_graph, load_graph
 
 class TestBuildGraph(unittest.TestCase):
     def test_build_graph(self):
@@ -10,5 +12,26 @@ class TestBuildGraph(unittest.TestCase):
         Only use training data to build a graph
         """
         dgraph = build_dynamic_graph("REDDIT")
-        print("done")
-    
+        df = load_graph("REDDIT")
+        train_edge_end = df[df['ext_roll'].gt(0)].index[0]
+        train_edge_end = df[df['ext_roll'].gt(0)].index[0]
+        srcs = np.array(df['src'][:train_edge_end], dtype=int)
+        srcs = np.unique(srcs)
+        
+        dsts = np.array(df['dst'][:train_edge_end], dtype=int)
+        dsts = np.unique(dsts)
+        
+        self.assertEqual(dgraph.num_vertices(), len(srcs) + len(dsts))
+        
+        # Test edges
+        for src in srcs:
+            df = df[:train_edge_end]
+            out_edges = np.array(df[df['src'] == src]['dst'], dtype=int)
+            ts = np.array(df[df['src'] == src]['time'])
+            ts = np.flip(ts)
+            
+            graph_out_edges, graph_ts = dgraph.out_edges(src)
+            self.assertEqual(len(out_edges), len(graph_out_edges))
+            self.assertEqual(len(graph_out_edges), dgraph.out_degree(src))
+            self.assertEqual(np.allclose(ts, graph_ts), True)
+        
