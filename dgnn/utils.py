041ef2c1cf280db05ca12d6fe3a902b3eaf7be4a
petersh6 <petershengwhu@gmail.com>
2022-05-06 12:14:09 +0800

    use features

---
+++ dgnn/utils.py
@@ -0,0 +1,54 @@
+import torch
+import dgl
+
+def prepare_input(mfgs, node_feats, edge_feats, combine_first=False, pinned=False, nfeat_buffs=None, efeat_buffs=None, nids=None, eids=None):
+    if combine_first:
+        for i in range(len(mfgs[0])):
+            if mfgs[0][i].num_src_nodes() > mfgs[0][i].num_dst_nodes():
+                num_dst = mfgs[0][i].num_dst_nodes()
+                ts = mfgs[0][i].srcdata['ts'][num_dst:]
+                nid = mfgs[0][i].srcdata['ID'][num_dst:].float()
+                nts = torch.stack([ts, nid], dim=1)
+                unts, idx = torch.unique(nts, dim=0, return_inverse=True)
+                uts = unts[:, 0]
+                unid = unts[:, 1]
+                # import pdb; pdb.set_trace()
+                b = dgl.create_block((idx + num_dst, mfgs[0][i].edges()[1]), num_src_nodes=unts.shape[0] + num_dst, num_dst_nodes=num_dst, device=torch.device('cuda:0'))
+                b.srcdata['ts'] = torch.cat([mfgs[0][i].srcdata['ts'][:num_dst], uts], dim=0)
+                b.srcdata['ID'] = torch.cat([mfgs[0][i].srcdata['ID'][:num_dst], unid], dim=0)
+                b.edata['dt'] = mfgs[0][i].edata['dt']
+                b.edata['ID'] = mfgs[0][i].edata['ID']
+                mfgs[0][i] = b
+    t_idx = 0
+    t_cuda = 0
+    i = 0
+    if node_feats is not None:
+        for b in mfgs[0]:
+            if pinned:
+                if nids is not None:
+                    idx = nids[i]
+                else:
+                    idx = b.srcdata['ID'].cpu().long()
+                torch.index_select(node_feats, 0, idx, out=nfeat_buffs[i][:idx.shape[0]])
+                b.srcdata['h'] = nfeat_buffs[i][:idx.shape[0]].cuda(non_blocking=True)
+                i += 1
+            else:
+                srch = node_feats[b.srcdata['ID'].long()].float()
+                b.srcdata['h'] = srch.cuda()
+    i = 0
+    if edge_feats is not None:
+        for mfg in mfgs:
+            for b in mfg:
+                if b.num_src_nodes() > b.num_dst_nodes():
+                    if pinned:
+                        if eids is not None:
+                            idx = eids[i]
+                        else:
+                            idx = b.edata['ID'].cpu().long()
+                        torch.index_select(edge_feats, 0, idx, out=efeat_buffs[i][:idx.shape[0]])
+                        b.edata['f'] = efeat_buffs[i][:idx.shape[0]].cuda(non_blocking=True)
+                        i += 1
+                    else:
+                        srch = edge_feats[b.edata['ID'].long()].float()
+                        b.edata['f'] = srch.cuda()
+    return mfgs
