4dd6291e50596ddf19da7a8942c381df13404ea5
yuchenzhong <izhongyuchen@gmail.com>
2022-07-05 11:24:27 +0800

    test build graph reverse

--- tests/test_build_graph.py
+++ tests/test_build_graph.py
@@ -6,6 +6,7 @@ from dgnn.utils import build_dynamic_graph, load_dataset
 
 
 class TestBuildGraph(unittest.TestCase):
+
     def test_build_graph(self):
         """
         Test building a dynamic graph from edges.csv(REDDIT)
@@ -29,6 +30,42 @@ class TestBuildGraph(unittest.TestCase):
             df = df[:train_edge_end]
             out_edges = np.array(df[df['src'] == src]['dst'], dtype=int)
             ts = np.array(df[df['src'] == src]['time'])
+            ts = np.flip(ts)
+
+            graph_out_edges, graph_ts, _ = dgraph.get_temporal_neighbors(src)
+            self.assertEqual(len(out_edges), len(graph_out_edges))
+            self.assertEqual(len(graph_out_edges), dgraph.out_degree(src))
+            self.assertTrue(np.allclose(ts, graph_ts))
+
+    def test_build_graph_add_reverse(self):
+        """
+        Test building a dynamic graph from edges.csv(REDDIT)
+        Only use training data to build a graph
+        """
+        train_df, _, _, df = load_dataset(dataset="REDDIT")
+        dgraph = build_dynamic_graph(train_df, add_reverse=True)
+
+        train_edge_end = df[df['ext_roll'].gt(0)].index[0]
+        srcs = np.array(df['src'][:train_edge_end], dtype=int)
+        srcs = np.unique(srcs)
+
+        dsts = np.array(df['dst'][:train_edge_end], dtype=int)
+        dsts = np.unique(dsts)
+
+        srcs = np.concatenate((srcs, dsts))
+
+        self.assertEqual(dgraph.num_vertices() - 1,
+                         max(np.max(srcs), np.max(dsts)))
+
+        # Test edges
+        for src in srcs:
+            df = df[:train_edge_end]
+            out_edges = np.array(df[df['src'] == src]['dst'], dtype=int)
+            out_edges_reverse = np.array(df[df['dst'] == src]['src'], dtype=int)
+            out_edges = np.concatenate((out_edges, out_edges_reverse))
+            ts = np.array(df[df['src'] == src]['time'])
+            ts_reverse = np.array(df[df['dst'] == src]['time'])
+            ts = np.concatenate((ts, ts_reverse))
             ts = np.flip(ts)
 
             graph_out_edges, graph_ts, _ = dgraph.get_temporal_neighbors(src)
