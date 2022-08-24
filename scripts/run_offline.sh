15a7196745ec27cf277eff50b4d226bb5cbefc76
petersh6 <petershengwhu@gmail.com>
2022-08-24 13:02:55 +0800

    fix

--- scripts/run_offline.sh
+++ scripts/run_offline.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-MODEL =$1
+MODEL=$1
 DATA=$2
 
 # TGAT
-if [$MODEL == "tgat"] | | [$MODEL == "TGAT"]
-then
-cmd = "python offline.py --model TGAT --dropout 0.1 --attn-dropout 0.1 \
+if [ $MODEL == "tgat" ] || [ $MODEL == "TGAT" ];then
+    cmd="python offline.py --model TGAT --dropout 0.1 --attn-dropout 0.1 \
                     --sample-layer 2 --sample-neighbor 10 10 \
                     --data REDDIT --sample-strategy uniform"
 fi
@@ -14,3 +13,7 @@ fi
 if [ $MODEL == "tgn" ] || [ $MODEL == "TGN" ];then
     cmd="python offline.py --dataset REDDIT --model TGN"
 fi
+
+
+echo $cmd
+exec $cmd
