3c819ca1f225420d878b46ba2fbedb440ca54a78
jasperzhong <izhongyuchen@gmail.com>
2022-09-08 13:32:33 +0000

    update

--- scripts/run_offline_multi_gpu.sh
+++ scripts/run_offline_multi_gpu.sh
@@ -3,14 +3,17 @@
 
 MODEL=$1
 DATA=$2
-NPROC_PER_NODE=${3:-1}
+CACHE="${3:-LFUCache}"
+CACHE_RATIO="${4:-0.2}" # default 20% of cache
+NPROC_PER_NODE=${5:-1}
 
 cmd="torchrun \
     --nnodes=1 --nproc_per_node=$NPROC_PER_NODE \
     --standalone \
-    offline_edge_prediction_multi_gpu.py --model $MODEL --data $DATA --profile"
+    offline_edge_prediction_multi_gpu.py --model $MODEL --data $DATA \
+    --cache $CACHE --cache-ratio $CACHE_RATIO"
 
 echo $cmd
-OMP_NUM_THREADS=8 exec $cmd
+OMP_NUM_THREADS=8 exec $cmd >> $MODEL-$DATA-$CACHE-$CACHE_RATIO.log 2>&1
 
 
