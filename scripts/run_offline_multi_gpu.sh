bf7648ec5e498e55c3fe0b5899bbcaf352fea6d0
jasperzhong <izhongyuchen@gmail.com>
2022-09-08 13:00:53 +0000

    profile

--- scripts/run_offline_multi_gpu.sh
+++ scripts/run_offline_multi_gpu.sh
@@ -8,7 +8,7 @@ NPROC_PER_NODE=${3:-1}
 cmd="torchrun \
     --nnodes=1 --nproc_per_node=$NPROC_PER_NODE \
     --standalone \
-    offline_edge_prediction_multi_gpu.py --model $MODEL --data $DATA "
+    offline_edge_prediction_multi_gpu.py --model $MODEL --data $DATA --profile"
 
 echo $cmd
 OMP_NUM_THREADS=8 exec $cmd
