b42600abf05e74c5141ce94d7f0653d3f56b61a4
jasperzhong <izhongyuchen@gmail.com>
2022-09-20 20:01:48 +0800

    add tests

--- tests/test_dynamic_graph.py
+++ tests/test_dynamic_graph.py
@@ -133,7 +133,8 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([2, 1, 0, 2, 1, 0, 2, 1, 0])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
         self.assertEqual(dgraph.num_edges(), 9)
         self.assertEqual(dgraph.num_vertices(), 4)
         self.assertEqual(dgraph.out_degree(0), 3)
@@ -180,7 +181,8 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
         self.assertEqual(dgraph.num_edges(), 9)
         self.assertEqual(dgraph.num_vertices(), 4)
         self.assertEqual(dgraph.out_degree(0), 3)
@@ -216,7 +218,8 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([3, 4, 5, 3, 4, 5, 3, 4, 5])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
         self.assertEqual(dgraph.num_edges(), 18)
         self.assertEqual(dgraph.num_vertices(), 4)
         self.assertEqual(dgraph.out_degree(0), 6)
@@ -264,7 +267,8 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
         self.assertEqual(dgraph.num_edges(), 9)
         self.assertEqual(dgraph.num_vertices(), 4)
         self.assertEqual(dgraph.out_degree(0), 3)
@@ -300,7 +304,8 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([3, 4, 5, 3, 4, 5, 3, 4, 5])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
         self.assertEqual(dgraph.num_edges(), 18)
         self.assertEqual(dgraph.num_vertices(), 4)
         self.assertEqual(dgraph.out_degree(0), 6)
@@ -345,7 +350,8 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 1, 2])
         target_vertices = np.array([1, 2, 3])
         timestamps = np.array([0, 1, 2])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
 
         source_vertices = np.array([0])
         target_vertices = np.array([1])
@@ -369,12 +375,14 @@ class TestDynamicGraph(unittest.TestCase):
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
 
         source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
         timestamps = np.array([3, 4, 5, 3, 4, 5, 3, 4, 5])
-        dgraph.add_edges(source_vertices, target_vertices, timestamps, add_reverse=False)
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, add_reverse=False)
 
         self.assertEqual(dgraph.num_edges(), 18)
         self.assertEqual(dgraph.num_vertices(), 4)
@@ -408,4 +416,120 @@ class TestDynamicGraph(unittest.TestCase):
         self.assertEqual(edge_ids.tolist(), [])
 
         print("Test replace insertion policy passed. (mem_resource_type: {})".format(
+            mem_resource_type))
+
+    @parameterized.expand(
+        itertools.product(["cuda", "unified", "pinned", "shared"]))
+    def test_add_edges_with_eids(self, mem_resource_type):
+        """
+        Test if the "replace" insertion policy works.
+        """
+        config = default_config.copy()
+        config["minimum_block_size"] = 4
+        config["mem_resource_type"] = mem_resource_type
+        dgraph = DynamicGraph(**config)
+        source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
+        target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+        timestamps = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2])
+        edge_ids = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, edge_ids, add_reverse=False)
+
+        source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
+        target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+        timestamps = np.array([3, 4, 5, 3, 4, 5, 3, 4, 5])
+        edge_ids = np.array([9, 10, 11, 12, 13, 14, 15, 16, 17])
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, edge_ids, add_reverse=False)
+
+        self.assertEqual(dgraph.num_edges(), 18)
+        self.assertEqual(dgraph.num_vertices(), 4)
+        self.assertEqual(dgraph.out_degree(0), 6)
+        self.assertEqual(dgraph.out_degree(1), 6)
+        self.assertEqual(dgraph.out_degree(2), 6)
+        self.assertEqual(dgraph.out_degree(3), 0)
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            0)
+        self.assertEqual(target_vertices.tolist(), [3, 2, 1, 3, 2, 1])
+        self.assertEqual(timestamps.tolist(), [5, 4, 3, 2, 1, 0])
+        self.assertEqual(edge_ids.tolist(), [11, 10, 9, 2, 1, 0])
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            1)
+        self.assertEqual(target_vertices.tolist(), [3, 2, 1, 3, 2, 1])
+        self.assertEqual(timestamps.tolist(), [5, 4, 3, 2, 1, 0])
+        self.assertEqual(edge_ids.tolist(), [14, 13, 12, 5, 4, 3])
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            2)
+        self.assertEqual(target_vertices.tolist(), [3, 2, 1, 3, 2, 1])
+        self.assertEqual(timestamps.tolist(), [5, 4, 3, 2, 1, 0])
+        self.assertEqual(edge_ids.tolist(), [17, 16, 15, 8, 7, 6])
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            3)
+        self.assertEqual(target_vertices.tolist(), [])
+        self.assertEqual(timestamps.tolist(), [])
+        self.assertEqual(edge_ids.tolist(), [])
+
+        print("Test add edges with eids passed. (mem_resource_type: {})".format(
+            mem_resource_type))
+
+    @parameterized.expand(
+        itertools.product(["cuda", "unified", "pinned", "shared"]))
+    def test_add_edges_with_noncontinous_eids(self, mem_resource_type):
+        """
+        Test if the "replace" insertion policy works.
+        """
+        config = default_config.copy()
+        config["minimum_block_size"] = 4
+        config["mem_resource_type"] = mem_resource_type
+        dgraph = DynamicGraph(**config)
+        source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
+        target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+        timestamps = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2])
+        edge_ids = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16])
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, edge_ids, add_reverse=False)
+
+        source_vertices = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
+        target_vertices = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+        timestamps = np.array([3, 4, 5, 3, 4, 5, 3, 4, 5])
+        edge_ids = np.array([17, 19, 21, 23, 25, 27, 29, 31, 33])
+        dgraph.add_edges(source_vertices, target_vertices,
+                         timestamps, edge_ids, add_reverse=False)
+
+        self.assertEqual(dgraph.num_edges(), 18)
+        self.assertEqual(dgraph.num_vertices(), 4)
+        self.assertEqual(dgraph.out_degree(0), 6)
+        self.assertEqual(dgraph.out_degree(1), 6)
+        self.assertEqual(dgraph.out_degree(2), 6)
+        self.assertEqual(dgraph.out_degree(3), 0)
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            0)
+        self.assertEqual(target_vertices.tolist(), [3, 2, 1, 3, 2, 1])
+        self.assertEqual(timestamps.tolist(), [5, 4, 3, 2, 1, 0])
+        self.assertEqual(edge_ids.tolist(), [21, 19, 17, 4, 2, 0])
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            1)
+        self.assertEqual(target_vertices.tolist(), [3, 2, 1, 3, 2, 1])
+        self.assertEqual(timestamps.tolist(), [5, 4, 3, 2, 1, 0])
+        self.assertEqual(edge_ids.tolist(), [27, 25, 23, 10, 8, 6])
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            2)
+        self.assertEqual(target_vertices.tolist(), [3, 2, 1, 3, 2, 1])
+        self.assertEqual(timestamps.tolist(), [5, 4, 3, 2, 1, 0])
+        self.assertEqual(edge_ids.tolist(), [33, 31, 29, 16, 14, 12])
+
+        target_vertices, timestamps, edge_ids = dgraph.get_temporal_neighbors(
+            3)
+        self.assertEqual(target_vertices.tolist(), [])
+        self.assertEqual(timestamps.tolist(), [])
+        self.assertEqual(edge_ids.tolist(), [])
+
+        print("Test add edges with non-continous eids passed. (mem_resource_type: {})".format(
             mem_resource_type))
