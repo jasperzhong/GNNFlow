8247416636240ffc42f179be91d30d11b5b2b3e5
jasperzhong <izhongyuchen@gmail.com>
2022-09-20 03:44:10 +0000

    add docs

--- dgnn/csrc/temporal_sampler.h
+++ dgnn/csrc/temporal_sampler.h
@@ -24,13 +24,19 @@ class TemporalSampler {
       const std::vector<TimestampType>& dst_timestamps);
 
   // TODO: implement this (refer to old `SampleLayer` implementation)
+  //
+  // NB: this function should handle input with dynamic length (i.e.,
+  // `dst_nodes` and `dst_timestamps` can have dynamic lengths every time). Make
+  // sure to re-allocate the buffer if the input size is larger than the current
+  // buffer size.
   SamplingResult SampleLayer(const std::vector<NIDType>& dst_nodes,
                              const std::vector<TimestampType>& dst_timestamps,
                              uint32_t layer, uint32_t snapshot);
 
   // the old `SampleLayer` implementation (remove this function)
   // std::vector<SamplingResult> SampleLayer(
-  //     uint32_t layer, const std::vector<SamplingResult>& prev_sampling_results);
+  //     uint32_t layer, const std::vector<SamplingResult>&
+  //     prev_sampling_results);
 
  private:
   std::vector<SamplingResult> RootInputToSamplingResult(
