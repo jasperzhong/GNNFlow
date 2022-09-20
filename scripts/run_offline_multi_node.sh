3f89eaeea52079d6b6fc5115b5efcca8b25629e8
jasperzhong <izhongyuchen@gmail.com>
2022-09-20 04:49:08 +0000

    update script

--- scripts/run_offline_multi_node.sh
+++ scripts/run_offline_multi_node.sh
@@ -6,9 +6,16 @@ CACHE="${3:-LFUCache}"
 CACHE_RATIO="${4:-0.2}" # default 20% of cache
 
 HOST_NODE_ADDR=10.28.1.31
-HOST_NODE_PORT=11211
+HOST_NODE_PORT=29400
 NNODES=2
 NPROC_PER_NODE=4
+
+CURRENT_NODE_IP=$(hostname -I | awk '{print $1}')
+if [ $CURRENT_NODE_IP = $HOST_NODE_ADDR ]; then
+    IS_HOST=true
+else
+    IS_HOST=false
+fi
 
 export NCCL_SOCKET_IFNAME=enp225s0
 
@@ -16,10 +23,11 @@ cmd="torchrun \
     --nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
     --rdzv_id=1234 --rdzv_backend=c10d \
     --rdzv_endpoint=$HOST_NODE_ADDR:$HOST_NODE_PORT \
+    --rdzv_conf is_host=$IS_HOST \
     offline_edge_prediction_multi_node.py --model $MODEL --data $DATA \
     --cache $CACHE --cache-ratio $CACHE_RATIO"
 
 echo $cmd
-OMP_NUM_THREADS=8 exec $cmd > $MODEL-$DATA-$CACHE-$CACHE_RATIO-$NNODES-$NPROC_PER_NODE.log 2>&1
+LOGLEVEL=INFO OMP_NUM_THREADS=8 exec $cmd 
 
 
