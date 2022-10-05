bba74bbba78a779ddd79b727244e47b1bc058f6b
jasperzhong <izhongyuchen@gmail.com>
2022-10-05 01:31:42 +0000

    fix

--- dgnn/distributed/dist_sampler.py
+++ dgnn/distributed/dist_sampler.py
@@ -202,7 +202,7 @@ class DistributedTemporalSampler:
             eids = sampling_result.eids
 
             mask = masks[i]
-            dst_idx = mask.nonzero(as_tuple=True)
+            dst_idx = mask.nonzero(as_tuple=True).numpy()
             all_row[offset:offset + num_edges] = dst_idx[sampling_result.row]
 
             all_dst_nodes[dst_idx] = dst_nodes
