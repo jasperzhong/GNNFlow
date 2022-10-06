f9bbc5c569e0d24cf54190ef97b8c87a28a5fba6
Richard <luck2qin@gmail.com>
2022-10-07 01:13:25 +0800

    fix

--- dgnn/distributed/partition.py
+++ dgnn/distributed/partition.py
@@ -333,7 +333,7 @@ class LDGPartitioner(Partitioner):
             if vid in self._neighbor_memory.keys():
                 neighbour_in_partition_size = len(self._neighbor_memory[vid][i])
 
-            partition_score.append(neighbour_in_partition_size - alpha * gamma * (partition_size ** (gamma - 1)))
+            partition_score.append(neighbour_in_partition_size - 0.04 * alpha * gamma * (partition_size ** (gamma - 1)))
 
         partition_score = np.array(partition_score)
 
