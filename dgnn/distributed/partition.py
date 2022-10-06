165e8e4dc2ae43c44f273cc8f62249e5520739d3
Richard <luck2qin@gmail.com>
2022-10-07 00:33:39 +0800

    add new ldg partitioner

--- dgnn/distributed/partition.py
+++ dgnn/distributed/partition.py
@@ -336,7 +336,7 @@ class LDGPartitioner(Partitioner):
 
         partition_score = np.array(partition_score)
 
-        if 1000 < vid < 2000:
+        if partition_score.max() > 0:
             print(partition_score)
 
         return np.random.choice(np.where(partition_score == partition_score.max())[0])
