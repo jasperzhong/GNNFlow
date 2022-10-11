4dba89fe97f24fa7d64e940f49d3db94bc2254b1
Richard <luck2qin@gmail.com>
2022-10-11 17:34:58 +0800

    update test

--- gnnflow/distributed/partition.py
+++ gnnflow/distributed/partition.py
@@ -161,8 +161,11 @@ class Partitioner:
             unique_src_nodes, dst_nodes_list, timestamps_list, eids_list)
 
         # restore partition table to the original src_nodes's size
+        print("before inverse_idx, the partitiontable is {}\n".format(partition_table))
         partition_table = partition_table[inverse_idx]
+        print("before the gather, the partition table is {}\n".format(partition_table))
         partition_table = partition_table.gather(0, sorted_idx.argsort(0))
+        print("finally the partititon table is {}\n".format(partition_table))
 
         return partition_table
 
