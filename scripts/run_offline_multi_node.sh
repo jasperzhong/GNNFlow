562db12a953e93a393d3f8a910d6fa1022f1f376
jasperzhong <izhongyuchen@gmail.com>
2022-10-13 12:16:32 +0800

    fix script

--- scripts/run_offline_multi_node.sh
+++ scripts/run_offline_multi_node.sh
@@ -28,7 +28,7 @@ cmd="torchrun \
     --rdzv_id=1234 --rdzv_backend=c10d \
     --rdzv_endpoint=$HOST_NODE_ADDR:$HOST_NODE_PORT \
     --rdzv_conf is_host=$IS_HOST \
-    offline_edge_prediction_multi_node_kvstore.py --model $MODEL --data $DATA \
+    offline_edge_prediction_multi_node.py --model $MODEL --data $DATA \
     --cache $CACHE --cache-ratio $CACHE_RATIO \
     --partition --ingestion-batch-size 100000 \
     --partition-strategy $PARTITION_STRATEGY"
