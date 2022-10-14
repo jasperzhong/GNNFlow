6e2743d3f50e98c9a613808dbfaa4aaf35e9a6b4
jasperzhong <izhongyuchen@gmail.com>
2022-10-14 15:22:53 +0800

    update

--- scripts/run_offline_multi_node.sh
+++ scripts/run_offline_multi_node.sh
@@ -15,16 +15,18 @@ NPROC_PER_NODE=1
 CURRENT_NODE_IP=$(ip -4 a show dev ${INTERFACE} | grep inet | cut -d " " -f6 | cut -d "/" -f1)
 if [ $CURRENT_NODE_IP = $HOST_NODE_ADDR ]; then
     IS_HOST=true
+    RANK=0
 else
     IS_HOST=false
+    RANK=1
 fi
 
 export NCCL_SOCKET_IFNAME=${INTERFACE}
 export GLOO_SOCKET_IFNAME=${INTERFACE}
 export TP_SOCKET_IFNAME=${INTERFACE}
 
-cmd="LOCAL_RANK=0 LOCAL_WORLD_SIZE=1 RANK=0 WORLD_SIZE=2 MASTER_ADDR=$HOST_NODE_ADDR MASTER_PORT=$HOST_NODE_PORT python \
-    offline_edge_prediction_multi_node.py --model $MODEL --data $DATA \
+cmd="LOCAL_RANK=$RANK LOCAL_WORLD_SIZE=1 RANK=$RANK WORLD_SIZE=2 MASTER_ADDR=$HOST_NODE_ADDR MASTER_PORT=$HOST_NODE_PORT \
+    python offline_edge_prediction_multi_node.py --model $MODEL --data $DATA \
     --cache $CACHE --cache-ratio $CACHE_RATIO \
     --partition --ingestion-batch-size 100000 \
     --partition-strategy $PARTITION_STRATEGY"
