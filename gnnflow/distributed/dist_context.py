757a376c7610a8befefddb65fcb85547dabf5cdb
yuchenzhong <izhongyuchen@gmail.com>
2022-10-24 10:29:51 +0000

    disable ib

--- gnnflow/distributed/dist_context.py
+++ gnnflow/distributed/dist_context.py
@@ -29,7 +29,10 @@ def initialize(rank: int, world_size: int, dataset: pd.DataFrame,
         data_name (str): the dataset name of the dataset for loading features.
         use_memory (bool): if the kvstore need to initialize the memory.
     """
-    rpc.init_rpc("worker%d" % rank, rank=rank, world_size=world_size)
+    rpc.init_rpc("worker%d" % rank, rank=rank, world_size=world_size,
+                 rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
+                     _transports=["shm", "uv"],
+                     _channels=["cma", "mpt_uv", "basic", "cuda_xth", "cuda_ipc", "cuda_basic"]))
     logging.info("Rank %d: Initialized RPC.", rank)
 
     local_rank = int(os.environ["LOCAL_RANK"])
