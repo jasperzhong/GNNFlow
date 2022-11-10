c0d12dd38ee3abe74ea73e42d648ef0ecd0ff4db
Richard <luck2qin@gmail.com>
2022-11-11 03:44:24 +0800

    cc

--- scripts/run_offline_multi_node_g4c.sh
+++ scripts/run_offline_multi_node_g4c.sh
@@ -9,7 +9,7 @@ PARTITION_STRATEGY="${5:-hash}"
 
 HOST_NODE_ADDR=172.31.35.208
 HOST_NODE_PORT=29400
-NNODES=2
+NNODES=3
 NPROC_PER_NODE=1
 
 CURRENT_NODE_IP=$(ip -4 a show dev ${INTERFACE} | grep inet | cut -d " " -f6 | cut -d "/" -f1)
