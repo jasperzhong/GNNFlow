f94cd9f9a7365bd5dd19bfb1a88d55a7825b3fad
Richard <luck2qin@gmail.com>
2022-11-11 09:19:02 +0800

    cc

--- gnnflow/distributed/dist_sampler.py
+++ gnnflow/distributed/dist_sampler.py
@@ -59,7 +59,6 @@ class DistributedTemporalSampler:
                 self._transform_output(ret, result)
 
                 callback(handle)
-            time.sleep(0.01)
 
     def _transform_output(self, input: SamplingResult, output: SamplingResultTorch):
         output.row = torch.from_numpy(input.row())
