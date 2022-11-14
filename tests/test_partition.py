054766ea8d346a542010de492fcf18a4f4052592
jasperzhong <izhongyuchen@gmail.com>
2022-11-14 11:13:08 +0800

    update

--- tests/test_partition.py
+++ tests/test_partition.py
@@ -5,11 +5,11 @@ import unittest
 import logging
 
 import numpy as np
-import pandas as pd
 import torch
 from parameterized import parameterized
 
 from gnnflow.distributed.partition import get_partitioner
+from gnnflow.utils import load_dataset
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -25,7 +25,7 @@ class TestPartition(unittest.TestCase):
         num_p = 4
         ingestion_batch_size = batch_size
         undirected = True
-        dataset = pd.read_csv('/home/ubuntu/data/{}/edges.csv'.format(dataset_name))  # LINUX
+        _, _, _, dataset = load_dataset(dataset_name)
         dataset.rename(columns={'Unnamed: 0': 'eid'}, inplace=True)
 
         num_nodes = 0
@@ -47,7 +47,8 @@ class TestPartition(unittest.TestCase):
             timestamps = batch["time"].values.astype(np.float32)
             eids = batch["eid"].values.astype(np.int64)
 
-            num_nodes = num_nodes + len(np.unique(np.concatenate([src_nodes, dst_nodes])))
+            num_nodes = num_nodes + \
+                len(np.unique(np.concatenate([src_nodes, dst_nodes])))
             num_edges = num_edges + len(eids)
 
             # undirected
@@ -59,25 +60,14 @@ class TestPartition(unittest.TestCase):
                 timestamps = np.concatenate([timestamps, timestamps])
                 eids = np.concatenate([eids, eids])
 
-                # src_nodes_ext = np.array(list(itertools.chain.from_iterable(zip(src_nodes, dst_nodes))))
-                # dst_nodes_ext = np.array(list(itertools.chain.from_iterable(zip(dst_nodes, src_nodes))))
-                # src_nodes = src_nodes_ext
-                # dst_nodes = dst_nodes_ext
-                #
-                # timestamps_ext = np.array(list(itertools.chain.from_iterable(zip(timestamps, timestamps))))
-                # timestamps = timestamps_ext
-                #
-                # eids_ext = np.array(list(itertools.chain.from_iterable(zip(eids, eids))))
-                # eids = eids_ext
-
             src_nodes = torch.from_numpy(src_nodes)
             dst_nodes = torch.from_numpy(dst_nodes)
             timestamps = torch.from_numpy(timestamps)
             eids = torch.from_numpy(eids)
 
-
             partition_start = time.time()
-            partitions = test_partitioner.partition(src_nodes, dst_nodes, timestamps, eids)
+            partitions = test_partitioner.partition(
+                src_nodes, dst_nodes, timestamps, eids)
             partition_end = time.time()
 
             for pt_idx in range(num_p):
@@ -91,28 +81,21 @@ class TestPartition(unittest.TestCase):
                 if ptablein[u] != -1 and ptablein[v] != -1 and (ptablein[u] != ptablein[v]):
                     edge_cut += 1
 
-            edge_cut_list.append(float(100.0 * float(edge_cut) / float(len(batch))))
+            edge_cut_list.append(
+                float(100.0 * float(edge_cut) / float(len(batch))))
             eid_list = torch.tensor([])
             for pid in range(num_p):
                 eid_list = torch.cat([eid_list, partitions[pid].eids])
-
-            # # check the rows
-            # for idx, row in batch.iterrows():
-            #     u = int(row['src'])
-            #     v = int(row['dst'])
-            #     e = int(row['eid'])
-            #
-            #     check_tensor = (eid_list == e).nonzero()
-            #     if len(check_tensor) == 0:
-            #         print("Find UNASSIGNED edge. u:{}, v:{}, eid:{}\n".format(u, v, e))
 
         # load balance
         ptable = test_partitioner.get_partition_table()
         psize_list = []
         for i in range(num_p):
             psize_list.append(ptable.tolist().count(i))
-            print("Partition {} has {} nodes.\n".format(i, ptable.tolist().count(i)))
-        load_factor = np.max(psize_list) / (np.min(psize_list) if np.min(psize_list) != 0 else 1)
+            print("Partition {} has {} nodes.\n".format(
+                i, ptable.tolist().count(i)))
+        load_factor = np.max(psize_list) / (np.min(psize_list)
+                                            if np.min(psize_list) != 0 else 1)
 
         for i in range(num_p):
             print("Partition {} has {} edges. \n".format(i, edge_num_tot[i]))
@@ -126,12 +109,13 @@ class TestPartition(unittest.TestCase):
         # Print Partition Table
         for i in range(len(ptable)):
             if ptable[i].item() >= num_p or ptable[i].item() == -1:
-                print("Incorrect Partition Table in vid {} is:{}\n".format(i, ptable[i].item()))
-
+                print("Incorrect Partition Table in vid {} is:{}\n".format(
+                    i, ptable[i].item()))
 
         print("Ptable is {}".format(ptable))
-        print("Total Time Usage: {} seconds\n".format(overall_end - overall_start))
+        print("Total Time Usage: {} seconds\n".format(
+            overall_end - overall_start))
         print("Load factor is:{} \n".format(load_factor))
         print("Edge Cut Percentage is :{}%;".format(np.average(edge_cut_list)))
-        print("========== Test Finished (DataSet:{}, Method:{}, BatchSize:{}, Assign_With_Dst:{}) =========\n\n".format(dataset_name, p_stgy, ingestion_batch_size, assign_with_dst))
-
+        print("========== Test Finished (DataSet:{}, Method:{}, BatchSize:{}, Assign_With_Dst:{}) =========\n\n".format(
+            dataset_name, p_stgy, ingestion_batch_size, assign_with_dst))
