b3fb1d124df79410c15be6a89ba47bd2ec669724
jasperzhong <izhongyuchen@gmail.com>
2022-11-22 09:13:07 +0000

    update script

--- scripts/offline_edge_prediction_multi_node_kvstore.py
+++ scripts/offline_edge_prediction_multi_node_kvstore.py
@@ -330,6 +330,7 @@ def train(train_loader, val_loader, sampler, model, optimizer, criterion,
         cache_edge_ratio_sum = 0
         cache_node_ratio_sum = 0
         total_samples = 0
+        cv_sampling_time = 0
 
         epoch_time_start = time.time()
         for i, (target_nodes, ts, eid) in enumerate(train_loader):
@@ -355,7 +356,6 @@ def train(train_loader, val_loader, sampler, model, optimizer, criterion,
             cache_node_ratio_sum += cache.cache_node_ratio
             total_samples += len(target_nodes)
 
-            std_sampling_time = 0
             if (i+1) % args.print_freq == 0:
                 if args.distributed:
                     metrics = torch.tensor([total_loss, cache_edge_ratio_sum,
@@ -367,11 +367,13 @@ def train(train_loader, val_loader, sampler, model, optimizer, criterion,
                         total_samples = metrics.tolist()
 
                     all_sampling_time = sampler.get_sampling_time()
-                    std_sampling_time = all_sampling_time.std(dim=0).mean()
+                    std = all_sampling_time.std(dim=0).mean()
+                    mean = all_sampling_time.mean(dim=0).mean()
+                    cv_sampling_time += std / mean
 
                 if args.rank == 0:
-                    logging.info('Epoch {:d}/{:d} | Iter {:d}/{:d} | Throughput {:.2f} samples/s | Loss {:.4f} | Cache node ratio {:.4f} | Cache edge ratio {:.4f} | std sampling time {:.4f}'.format(e + 1, args.epoch, i + 1, int(len(
-                        train_loader)/args.world_size), total_samples * args.world_size / (time.time() - epoch_time_start), total_loss / (i + 1), cache_node_ratio_sum / (i + 1), cache_edge_ratio_sum / (i + 1), std_sampling_time))
+                    logging.info('Epoch {:d}/{:d} | Iter {:d}/{:d} | Throughput {:.2f} samples/s | Loss {:.4f} | Cache node ratio {:.4f} | Cache edge ratio {:.4f} | sampling time CV {:.4f}'.format(e + 1, args.epoch, i + 1, int(len(
+                        train_loader)/args.world_size), total_samples * args.world_size / (time.time() - epoch_time_start), total_loss / (i + 1), cache_node_ratio_sum / (i + 1), cache_edge_ratio_sum / (i + 1), cv_sampling_time))
 
         epoch_time = time.time() - epoch_time_start
         epoch_time_sum += epoch_time
@@ -390,7 +392,6 @@ def train(train_loader, val_loader, sampler, model, optimizer, criterion,
         val_end = time.time()
         val_time = val_end - val_start
 
-        std_sampling_time = 0
         if args.distributed:
             metrics = torch.tensor([val_ap, val_auc, cache_edge_ratio_sum,
                                     cache_node_ratio_sum, total_samples], device=device)
@@ -400,11 +401,13 @@ def train(train_loader, val_loader, sampler, model, optimizer, criterion,
                 total_samples = metrics.tolist()
 
             all_sampling_time = sampler.get_sampling_time()
-            std_sampling_time = all_sampling_time.std(dim=0).mean()
+            std = all_sampling_time.std(dim=0).mean()
+            mean = all_sampling_time.mean(dim=0).mean()
+            cv_sampling_time += std / mean
 
         if args.rank == 0:
-            logging.info("Epoch {:d}/{:d} | Validation ap {:.4f} | Validation auc {:.4f} | Train time {:.2f} s | Validation time {:.2f} s | Train Throughput {:.2f} samples/s | Cache node ratio {:.4f} | Cache edge ratio {:.4f} | std sampling time {:.4f}".format(
-                e + 1, args.epoch, val_ap, val_auc, epoch_time, val_time, total_samples * args.world_size / epoch_time, cache_node_ratio_sum / (i + 1), cache_edge_ratio_sum / (i + 1), std_sampling_time))
+            logging.info("Epoch {:d}/{:d} | Validation ap {:.4f} | Validation auc {:.4f} | Train time {:.2f} s | Validation time {:.2f} s | Train Throughput {:.2f} samples/s | Cache node ratio {:.4f} | Cache edge ratio {:.4f} | sampling time CV {:.4f}".format(
+                e + 1, args.epoch, val_ap, val_auc, epoch_time, val_time, total_samples * args.world_size / epoch_time, cache_node_ratio_sum / (i + 1), cache_edge_ratio_sum / (i + 1), cv_sampling_time))
 
         if args.rank == 0 and e > 1 and val_ap > best_ap:
             best_e = e + 1
