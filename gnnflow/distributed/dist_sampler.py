294018b798525123c47d02e601b020253686170b
jasperzhong <izhongyuchen@gmail.com>
2022-11-23 08:36:16 +0000

    update

--- gnnflow/distributed/dist_sampler.py
+++ gnnflow/distributed/dist_sampler.py
@@ -340,17 +340,16 @@ class DistributedTemporalSampler:
         assert self._local_rank == 0
 
         with self._load_table_lock:
-            load_table = self._load_table.clone()
+            load_table = self._load_table
 
-        weight = load_table.sum(dim=0, keepdim=True) / load_table
-        weight = torch.softmax(weight, dim=0)
-        min_load_local_rank = int(torch.multinomial(weight, 1).item())
-        min_load_global_rank = min_load_local_rank + \
-            self._partition_id*self._local_world_size
+            weight = load_table.sum(dim=0, keepdim=True) / load_table
+            weight = torch.softmax(weight, dim=0)
+            min_load_local_rank = int(torch.multinomial(weight, 1).item())
+            min_load_global_rank = min_load_local_rank + \
+                self._partition_id*self._local_world_size
 
-        # update load table
-        load = len(target_vertices)
-        with self._load_table_lock:
+            # update load table
+            load = len(target_vertices)
             self._load_table[min_load_local_rank] += load
 
         if min_load_global_rank == self._rank:
