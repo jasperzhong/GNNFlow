41256e6f65b29e6b52a1ae106cbf4f750f3cef37
jasperzhong <izhongyuchen@gmail.com>
2022-11-24 08:19:43 +0000

    update

--- scripts/offline_edge_prediction_multi_node_kvstore.py
+++ scripts/offline_edge_prediction_multi_node_kvstore.py
@@ -375,7 +375,7 @@ def train(train_loader, val_loader, sampler, model, optimizer, criterion,
 
                 if args.rank == 0:
                     logging.info('Epoch {:d}/{:d} | Iter {:d}/{:d} | Throughput {:.2f} samples/s | Loss {:.4f} | Cache node ratio {:.4f} | Cache edge ratio {:.4f} | avg sampling time CV {:.4f}'.format(e + 1, args.epoch, i + 1, int(len(
-                        train_loader)/args.world_size), total_samples * args.world_size / (time.time() - epoch_time_start), total_loss / (i + 1), cache_node_ratio_sum / (i + 1), cache_edge_ratio_sum / (i + 1), cv_sampling_time / ((i+1)/args.print_freq)))
+                        train_loader)), total_samples * args.world_size / (time.time() - epoch_time_start), total_loss / (i + 1), cache_node_ratio_sum / (i + 1), cache_edge_ratio_sum / (i + 1), cv_sampling_time / ((i+1)/args.print_freq)))
                         
         epoch_time = time.time() - epoch_time_start
         epoch_time_sum += epoch_time
