65d11c1259c77feb47ea4d7b2055be98fd55b2a8
Richard <luck2qin@gmail.com>
2022-11-25 01:11:03 +0800

    debug

--- tests/test_partition.py
+++ tests/test_partition.py
@@ -17,10 +17,10 @@ logging.basicConfig(level=logging.DEBUG)
 class TestPartition(unittest.TestCase):
 
     @parameterized.expand(
-        itertools.product(["hash", "fennel", "fennel_edge"], [10000], [10000], [False]))
+        itertools.product(["dgl_metis"], [672447], [100000], [False]))
     def test_partition_graph(self, partition_strategy, initial_ingestion_batch_size, ingestion_batch_size, assign_with_dst):
 
-        dataset_name = 'GDELT'
+        dataset_name = 'REDDIT'
         p_stgy = partition_strategy
         num_p = 4
         undirected = True
