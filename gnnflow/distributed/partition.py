5cc74136248bd15c63f443f3109f290ea6a21b18
Richard <luck2qin@gmail.com>
2022-11-24 21:34:41 +0800

    cc

--- gnnflow/distributed/partition.py
+++ gnnflow/distributed/partition.py
@@ -812,7 +812,7 @@ class DGLMetisPartitioner(Partitioner):
         for i in range(len(unique_src_nodes)):
             for j in range(len(dst_nodes_list[i])):
                 src_list.append(unique_src_nodes[i])
-                dst_list.append(dst_nodes_list[j])
+                dst_list.append(dst_nodes_list[i][j])
 
         src_list = np.array(src_list)
         dst_list = np.array(dst_list)
