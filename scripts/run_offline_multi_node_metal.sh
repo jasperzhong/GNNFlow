fc67d49b363c401687f4e3b1866bb9381b3b3b5d
jasperzhong <izhongyuchen@gmail.com>
2022-11-24 07:38:20 +0000

    fix partition node feature bug

--- scripts/run_offline_multi_node_metal.sh
+++ scripts/run_offline_multi_node_metal.sh
@@ -12,7 +12,7 @@ CHUNKS="${7:-1}"
 HOST_NODE_ADDR=172.31.44.144
 HOST_NODE_PORT=29400
 NNODES=4
-NPROC_PER_NODE=8
+NPROC_PER_NODE=1
 
 CURRENT_NODE_IP=$(ip -4 a show dev ${INTERFACE} | grep inet | cut -d " " -f6 | cut -d "/" -f1)
 if [ $CURRENT_NODE_IP = $HOST_NODE_ADDR ]; then
