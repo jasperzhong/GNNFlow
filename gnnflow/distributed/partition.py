48f69f0bced8781f6a9500e3dedfc8f6b4b8496c
Richard <luck2qin@gmail.com>
2022-11-24 11:08:22 +0800

    cc

--- gnnflow/distributed/partition.py
+++ gnnflow/distributed/partition.py
@@ -541,7 +541,7 @@ class FennelEdgePartitioner(Partitioner):
             bal_score.append(self._edges_partitioned_num_list[i].item())
             loc_score.append(locality_score)
 
-            partition_score.append(locality_score - (1.0 / 100000.0) * (self._edges_partitioned_num_list[i]))
+            partition_score.append(locality_score - (self._num_partitions) * (self._edges_partitioned_num_list[i] / self._edges_partitioned))
 
         partition_score = np.array(partition_score)
 
