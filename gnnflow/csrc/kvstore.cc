ddb6a77b5f43f7bd6093c4c154835f7ef0253c3e
jasperzhong <izhongyuchen@gmail.com>
2022-11-29 18:46:41 +0800

    update cpp kvstore

--- gnnflow/csrc/kvstore.cc
+++ gnnflow/csrc/kvstore.cc
@@ -1,8 +1,5 @@
 #include "kvstore.h"
 
-#include <chrono>
-#include <iostream>
-
 #include "utils.h"
 
 namespace gnnflow {
@@ -17,39 +14,16 @@ void KVStore::set(const std::vector<Key>& keys, const at::Tensor& values) {
 at::Tensor KVStore::get(const std::vector<Key>& keys) {
   auto size = keys.size();
   // sort the keys
-  // auto indices = stable_sort_indices(keys);
-  // auto sorted_keys = sort_vector(keys, indices);
+  auto indices = stable_sort_indices(keys);
+  auto sorted_keys = sort_vector(keys, indices);
 
-  auto start = std::chrono::system_clock::now();
   std::vector<at::Tensor> values(size);
   for (size_t i = 0; i < size; ++i) {
-    values[i] = store_[keys[i]];
-    // values[indices[i]] = store_[sorted_keys[i]];
-  }
-  {
-    std::lock_guard<std::mutex> lock(mutex_);
-    lookup_time_ += std::chrono::duration_cast<std::chrono::microseconds>(
-                        std::chrono::system_clock::now() - start)
-                        .count() /
-                    1000.0;
+    values[indices[i]] = store_[sorted_keys[i]];
   }
 
-  start = std::chrono::system_clock::now();
   // cat then reshape
   auto tensor = at::cat(values).reshape({static_cast<int64_t>(size), -1});
-
-  {
-    std::lock_guard<std::mutex> lock(mutex_);
-    stack_time_ += std::chrono::duration_cast<std::chrono::microseconds>(
-                       std::chrono::system_clock::now() - start)
-                       .count() /
-                   1000.0;
-  }
-
-  std::cout << "lookup time: " << lookup_time_ << " ms"
-            << "\tstack time: " << stack_time_ << " ms"
-            << "\tstack ratio: " << stack_time_ / (lookup_time_ + stack_time_)
-            << std::endl;
 
   return tensor;
 }
