9cb7f5e2e708c14adf204be59d4544abe0d6b4a7
jasperzhong <izhongyuchen@gmail.com>
2022-11-29 16:31:40 +0800

    disable parallel read

--- gnnflow/csrc/kvstore.cc
+++ gnnflow/csrc/kvstore.cc
@@ -11,7 +11,7 @@ void KVStore::set(const std::vector<Key>& keys, const at::Tensor& values) {
 at::Tensor KVStore::get(const std::vector<Key>& keys) const {
   const auto size = keys.size();
   std::vector<at::Tensor> values(size);
-#pragma omp parallel for num_threads(num_threads_) schedule(static)
+  // #pragma omp parallel for num_threads(num_threads_) schedule(static)
   for (size_t i = 0; i < size; ++i) {
     values[i] = store_.at(keys[i]);
   }
