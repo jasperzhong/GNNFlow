cea5710c9b3c67a52ad298d34f28e4fd5a04501e
jasperzhong <izhongyuchen@gmail.com>
2022-12-04 13:51:25 +0000

    batch size = 2k

--- gnnflow/config.py
+++ gnnflow/config.py
@@ -163,7 +163,7 @@ _gdelt_default_config = {
     "undirected": False,
     "node_feature": True,
     "edge_feature": True,
-    "batch_size": 4000
+    "batch_size": 2000
 }
 
 _mag_default_config = {
