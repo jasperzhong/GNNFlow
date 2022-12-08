b8d1074ba3a621c73785e7f2c951de694a19c6b2
jasperzhong <izhongyuchen@gmail.com>
2022-12-08 16:02:21 +0800

    optimize

--- gnnflow/distributed/dispatcher.py
+++ gnnflow/distributed/dispatcher.py
@@ -222,11 +222,19 @@ class Dispatcher:
     def broadcast_rand_sampler(self):
         train_dst_set = torch.LongTensor(list(self._train_dst_set))
         nontrain_dst_set = torch.LongTensor(list(self._nontrain_dst_set))
+        futures = []
         for partition_id in range(self._num_partitions):
             for worker_id in range(self._local_world_size):
                 worker_rank = partition_id * self._local_world_size + worker_id
-                rpc.rpc_sync("worker%d" % worker_rank, graph_services.set_rand_sampler,
-                             args=(train_dst_set, nontrain_dst_set))
+                if worker_rank == self._rank:
+                    graph_services.set_rand_sampler(
+                        train_dst_set, nontrain_dst_set)
+                else:
+                    futures.append(rpc.rpc_async("worker%d" % worker_rank, graph_services.set_rand_sampler,
+                                                 args=(train_dst_set, nontrain_dst_set)))
+
+        for future in futures:
+            future.wait()
 
 
 def get_dispatcher(partition_strategy: Optional[str] = None, num_partitions: Optional[int] = None,
