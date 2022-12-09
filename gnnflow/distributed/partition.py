2241da728191057bed05a8f058f0f385fa40bcc2
Richard <luck2qin@gmail.com>
2022-12-09 20:51:33 +0800

    update

--- gnnflow/distributed/partition.py
+++ gnnflow/distributed/partition.py
@@ -725,8 +725,8 @@ class FennelEdgePartitioner(Partitioner):
         load_balance_score = []
 
         for i in range(self._num_partitions):
-            if self._edges_partitioned_num_list[i] > 1.3 * (self._edges_partitioned / self._num_partitions):
-                partition_score.append(-100000)
+            if self._edges_partitioned_num_list[i] > 1.2 * (self._edges_partitioned / self._num_partitions):
+                partition_score.append(-10000)
                 load_balance_score.append(-10)
                 continue
 
@@ -746,7 +746,7 @@ class FennelEdgePartitioner(Partitioner):
             load_balance_score.append(self._edges_partitioned_num_list[i] / self._edges_partitioned)
             partition_score.append(locality_score)
 
-        load_balance_score = self.edge_set_normalize(load_balance_score, -100, 100)
+        load_balance_score = self.edge_set_normalize(load_balance_score, -10, 10)
 
         for i in range(self._num_partitions):
             partition_score[i] += load_balance_score[i]
