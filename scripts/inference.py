80d71bc38d40e85eaa054eb6f274fae4ac1fcc6a
yuchenzhong <izhongyuchen@gmail.com>
2023-01-12 10:12:27 +0000

    update

--- scripts/inference.py
+++ scripts/inference.py
@@ -10,8 +10,9 @@ import torch.distributed
 import torch.nn
 import torch.nn.parallel
 import torch.utils.data
-from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import TSNE
+from sklearn.metrics import average_precision_score, roc_auc_score
+from sklearn.preprocessing import StandardScaler
 
 import gnnflow.cache as caches
 from gnnflow.config import get_default_config
@@ -19,10 +20,10 @@ from gnnflow.models.dgnn import DGNN
 from gnnflow.models.gat import GAT
 from gnnflow.models.graphsage import SAGE
 from gnnflow.temporal_sampler import TemporalSampler
-from gnnflow.utils import (build_dynamic_graph, get_batch_no_neg,
+from gnnflow.utils import (DstRandEdgeSampler, build_dynamic_graph,
+                           get_batch_no_neg, get_batch,
                            get_pinned_buffers, get_project_root_dir,
-                           load_dataset, load_feat, mfgs_to_cuda,
-                           prepare_input)
+                           load_dataset, load_feat, mfgs_to_cuda)
 
 datasets = ['REDDIT', 'GDELT', 'LASTFM', 'MAG', 'MOOC', 'WIKI']
 model_names = ['TGN', 'TGAT', 'DySAT', 'GRAPHSAGE', 'GAT']
@@ -64,6 +65,41 @@ def set_seed(seed):
 set_seed(args.seed)
 
 
+def evaluate(df, sampler, model, cache, device, rand_edge_sampler):
+    model.eval()
+    aps = list()
+    aucs_mrrs = list()
+
+    with torch.no_grad():
+        total_loss = 0
+        for target_nodes, ts, eid in get_batch(df=df,
+                                               batch_size=args.batch_size,
+                                               num_chunks=0,
+                                               rand_edge_sampler=rand_edge_sampler,
+                                               world_size=args.world_size):
+            mfgs = sampler.sample(target_nodes, ts)
+            mfgs_to_cuda(mfgs, device)
+            mfgs = cache.fetch_feature(
+                mfgs, eid)
+            pred_pos, pred_neg = model(mfgs)
+
+            if args.use_memory:
+                model.memory.update_mem_mail(
+                    **model.last_updated, edge_feats=cache.target_edge_features,
+                    neg_sample_ratio=1)
+
+            y_pred = torch.cat([pred_pos, pred_neg], dim=0).sigmoid().cpu()
+            y_true = torch.cat(
+                [torch.ones(pred_pos.size(0)),
+                 torch.zeros(pred_neg.size(0))], dim=0)
+            aucs_mrrs.append(roc_auc_score(y_true, y_pred))
+            aps.append(average_precision_score(y_true, y_pred))
+
+    ap = float(torch.tensor(aps).mean())
+    auc_mrr = float(torch.tensor(aucs_mrrs).mean())
+    return ap, auc_mrr
+
+
 def main():
     args.distributed = False
     args.local_rank = args.rank = 0
@@ -72,6 +108,8 @@ def main():
     args.use_memory = model_config['use_memory']
 
     _, _, test_data, full_data = load_dataset(args.data)
+    test_rand_sampler = DstRandEdgeSampler(full_data['dst'].to_numpy())
+
     dgraph = build_dynamic_graph(
         **data_config, device=args.local_rank, dataset_df=full_data)
 
@@ -79,6 +117,7 @@ def main():
     num_edges = dgraph.num_edges()
 
     batch_size = 4000
+    args.batch_size = batch_size
     node_feats, edge_feats = load_feat(
         args.data, shared_memory=args.distributed,
         local_rank=args.local_rank, local_world_size=args.local_world_size)
@@ -134,10 +173,15 @@ def main():
     def get_embed(snapshot_time_window, repeat=10):
         embeds_list = []
         model.eval()
-        for _ in range(repeat):
-            embeds = []
-            model_config['snapshot_time_window'] = snapshot_time_window
-            with torch.no_grad():
+        model_config['snapshot_time_window'] = snapshot_time_window
+        sampler = TemporalSampler(dgraph, **model_config)
+        with torch.no_grad():
+            if args.use_memory:
+                memory_backup = model.memory.backup()
+            for _ in range(repeat):
+                embeds = []
+                if args.use_memory:
+                    model.memory.restore(memory_backup)
                 for target_nodes, ts, eid in get_batch_no_neg(test_data, batch_size):
                     mfgs = sampler.sample(target_nodes, ts)
 
@@ -155,13 +199,16 @@ def main():
                                 **model.last_updated, edge_feats=cache.target_edge_features,
                                 neg_sample_ratio=0)
 
-            embeds = np.concatenate(embeds, axis=0)[-3000:]
-            embeds_list.append(embeds)
-        return torch.from_numpy(np.mean(embeds_list, axis=0))
+                embeds = np.concatenate(embeds, axis=0)[-3000:]
+                embeds_list.append(embeds)
 
-    embed1 = get_embed(0)
-    embed2 = get_embed(86400)
-    embed3 = get_embed(3600)
+        ap, auc = evaluate(
+            test_data, sampler, model, cache, device, test_rand_sampler)
+        return torch.from_numpy(np.mean(embeds_list, axis=0)), ap, auc
+
+    embed1, ap1, auc1 = get_embed(0)
+    embed2, ap2, auc2 = get_embed(86400)
+    embed3, ap3, auc3 = get_embed(3600)
 
     # scale
     embed1 = StandardScaler().fit_transform(embed1).astype(np.float32)
@@ -181,6 +228,8 @@ def main():
                 label='sliding time window (T=1hr)', s=5)
     plt.legend()
     plt.savefig(f"{args.model}_{args.data}.png", dpi=400, bbox_inches='tight')
+
+    print(f"full data AP: {ap1:.4f}, AUC: {auc1:.4f}\nsliding time window (T=1d) AP: {ap2:.4f}, AUC: {auc2:.4f}\nsliding time window (T=1hr) AP: {ap3:.4f}, AUC: {auc3:.4f}")
 
 
 if __name__ == "__main__":
