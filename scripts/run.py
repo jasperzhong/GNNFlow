ae5e7b8466053ac883018f680d906c4d0aea3fcf
vycezhong <izhongyuchen@gmail.com>
2023-03-19 18:45:06 +0800

    update

---
+++ scripts/run.py
@@ -0,0 +1,12 @@
+import itertools
+import os
+
+models = ['TGN', 'TGAT', 'GRAPHSAGE']
+datasets = ['WIKI', 'REDDIT', 'MOOC', 'LASTFM']
+cache = ['LRUCache', 'LFUCache', 'FIFOCache', 'GNNLabStaticCache']
+param_space = itertools.product(
+    models, datasets, cache)
+
+for param in param_space:
+    os.system("python3 offline_edge_prediction.py --model {} --data {} --epoch 10 --cache {} --edge-cache-ratio 0.2 --node-cache-ratio 0".format(*param))
+
