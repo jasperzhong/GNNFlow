87935a3bffbc7c241a59c8921cfebf5941bd841e
jasperzhong <izhongyuchen@gmail.com>
2023-04-27 12:01:51 +0000

    add multi node training script

--- scripts/run_offline_multi_node_metal_2.sh
+++ scripts/run_offline_multi_node_metal_2.sh
@@ -6,12 +6,10 @@ DATA=$2
 CACHE="${3:-LFUCache}"
 EDGE_CACHE_RATIO="${4:-0.2}" # default 20% of cache
 NODE_CACHE_RATIO="${5:-0.2}" # default 20% of cache
-PARTITION_STRATEGY="${6:-hash}"
-DYNAMIC_SCHEDULING="${7:-0}"
 
-HOST_NODE_ADDR=172.31.30.101
+HOST_NODE_ADDR=172.31.27.162
 HOST_NODE_PORT=29400
-NNODES=4
+NNODES=2
 NPROC_PER_NODE=8
 
 CURRENT_NODE_IP=$(ip -4 a show dev ${INTERFACE} | grep inet | cut -d " " -f6 | cut -d "/" -f1)
@@ -30,19 +28,12 @@ cmd="torchrun \
     --rdzv_id=1234 --rdzv_backend=c10d \
     --rdzv_endpoint=$HOST_NODE_ADDR:$HOST_NODE_PORT \
     --rdzv_conf is_host=$IS_HOST \
-    offline_edge_prediction_multi_node_kvstore.py --model $MODEL --data $DATA \
+    offline_edge_prediction.py --model $MODEL --data $DATA \
     --cache $CACHE --edge-cache-ratio $EDGE_CACHE_RATIO --node-cache-ratio $NODE_CACHE_RATIO\
-    --partition --ingestion-batch-size 10000000 \
-    --initial-ingestion-batch-size 100000000 \
-    --partition-strategy $PARTITION_STRATEGY \
-    --num-workers 0"
-
-if [ $DYNAMIC_SCHEDULING = 1 ]; then
-    cmd="$cmd --dynamic-scheduling"
-fi
+    --partition --ingestion-batch-size 10000000"
 
 rm -rf /dev/shm/rmm_pool_*
 
 echo $cmd
-LOGLEVEL=INFO OMP_NUM_THREADS=8 exec $cmd
+OMP_NUM_THREADS=8 exec $cmd > ${MODEL}_${DATA}_${CACHE}_${EDGE_CACHE_RATIO}_${NODE_CACHE_RATIO}_${NNODES}_${NPROC_PER_NODE}.log 2>&1
 
