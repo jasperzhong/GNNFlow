122bc7321e769a348ba9d9736d219ec2f347d0c5
jasperzhong <izhongyuchen@gmail.com>
2023-04-30 11:23:45 +0000

    fix typo

--- benchmarks/run.py
+++ benchmarks/run.py
@@ -7,4 +7,4 @@ strategies = ['naive',  'fix', 'lineardeg_adaptive']
 
 for model, dataset, strategy in itertools.product(models, datasets, strategies):
     os.system(
-        f'python benchmark_sampler.py --model {model} --dataset {dataset} --adaptive-block-size-strategy {strategy}")
+        f'python benchmark_sampler.py --model {model} --dataset {dataset} --adaptive-block-size-strategy {strategy}')
